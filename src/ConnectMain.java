2977a0511571acae184b3bfc3932b8b9557c21c6
soumyagh <soumyagh@thoughtworks.com>
2014-01-25 12:39:15 +0530

    connector created

---
+++ src/ConnectMain.java
@@ -0,0 +1,11 @@
+/**
+ * Created by soumyagh on 1/25/14.
+ */
+public class ConnectMain {
+    public static void main(String[] args) {
+        Connector soumya = new Connector();
+        soumya.connect();
+        System.out.println("HelloWorld");
+
+    }
+}
